6bd39fc3e8b9c7628e644c20c8c4c7f383cfe455
JoaoManuelMarquesChaves <119414658+JoaoManuelMarquesChaves@users.noreply.github.com>
2024-02-02 11:18:16 +0100

    Update app.c

--- TP2_UART/TP1_PWM 2/firmware/src/app.c
+++ TP2_UART/TP1_PWM 2/firmware/src/app.c
@@ -96,25 +96,25 @@ S_pwmSettings PwmDataToSend;
 /* Fonction :
     void callback_timer1(void)
 
-  Résumé :
+  RÃ©sumÃ© :
     Fonction de rappel du timer 1.
 
   Description :
-    Cette fonction est appelée à chaque déclenchement du timer 1, qui est
-    configuré pour des déclenchements tous les 100 ms. Elle maintient un
-    compteur statique, et lorsqu'il atteint la valeur 30 (soit après 3 secondes),
-    elle déclenche un changement d'état de l'application vers APP_STATE_SERVICE_TASKS
+    Cette fonction est appelÃ©e Ã  chaque dÃ©clenchement du timer 1, qui est
+    configurÃ© pour des dÃ©clenchements tous les 100 ms. Elle maintient un
+    compteur statique, et lorsqu'il atteint la valeur 30 (soit aprÃ¨s 3 secondes),
+    elle dÃ©clenche un changement d'Ã©tat de l'application vers APP_STATE_SERVICE_TASKS
     en appelant la fonction APP_UpdateState elle reappelle cette tout les 100ms 
 
   Remarques :
-    - La fonction est probablement associée à un timer configuré pour des
-      déclenchements périodiques tous les 100 ms.
+    - La fonction est probablement associÃ©e Ã  un timer configurÃ© pour des
+      dÃ©clenchements pÃ©riodiques tous les 100 ms.
 
 */
 // *****************************************************************************
 void callback_timer1(void)
 {
-        // Déclencher un changement d'état vers APP_STATE_SERVICE_TASKS
+        // DÃ©clencher un changement d'Ã©tat vers APP_STATE_SERVICE_TASKS
         APP_UpdateState(APP_STATE_SERVICE_TASKS);
 }
 
@@ -129,29 +129,29 @@ void callback_timer1(void)
 /* Fonction :
     void APP_UpdateState(APP_STATES NewState)
 
-  Résumé :
-    Met à jour l'état global de l'application.
+  RÃ©sumÃ© :
+    Met Ã  jour l'Ã©tat global de l'application.
 
   Description :
-    Cette fonction met à jour l'état global du switch avec la nouvelle
-    valeur spécifiée en tant que paramètre. Elle doit être appelée pour changer
-    l'état de l'application selon les besoins de la logique de la machine à états.
+    Cette fonction met Ã  jour l'Ã©tat global du switch avec la nouvelle
+    valeur spÃ©cifiÃ©e en tant que paramÃ¨tre. Elle doit Ãªtre appelÃ©e pour changer
+    l'Ã©tat de l'application selon les besoins de la logique de la machine Ã  Ã©tats.
 
   Remarques :
-    La variable globale appData.state est mise à jour directement avec la nouvelle
-    valeur spécifiée en paramètre. Aucune valeur de retour n'est fournie, car la
-    modification est effectuée directement sur la variable globale d'état.
+    La variable globale appData.state est mise Ã  jour directement avec la nouvelle
+    valeur spÃ©cifiÃ©e en paramÃ¨tre. Aucune valeur de retour n'est fournie, car la
+    modification est effectuÃ©e directement sur la variable globale d'Ã©tat.
 
-    Cette fonction est utilisée pour faciliter la gestion de l'état de l'application
-    dans la machine à états principale de l'application.
+    Cette fonction est utilisÃ©e pour faciliter la gestion de l'Ã©tat de l'application
+    dans la machine Ã  Ã©tats principale de l'application.
 */
 // *****************************************************************************
 void APP_UpdateState(APP_STATES NewState)
 {
-    // Met à jour l'état de l'application avec la nouvelle valeur
+    // Met Ã  jour l'Ã©tat de l'application avec la nouvelle valeur
     appData.state = NewState;
     
-    // Aucune sortie explicite, car la mise à jour est effectuée directement sur la variable d'état globale.
+    // Aucune sortie explicite, car la mise Ã  jour est effectuÃ©e directement sur la variable d'Ã©tat globale.
     // La fonction n'a pas de valeur de retour (void).
 }
 
@@ -162,24 +162,24 @@ void APP_UpdateState(APP_STATES NewState)
 /* Fonction :
     void Allume_Leds(uint8_t ChoixLed)
 
-  Résumé :
-    Contrôle l'état des LEDs en fonction du masque de choix spécifié.
+  RÃ©sumÃ© :
+    ContrÃ´le l'Ã©tat des LEDs en fonction du masque de choix spÃ©cifiÃ©.
 
   Description :
-    Cette fonction prend en entrée un masque de bits (ChoixLed) où chaque bit
-    correspond à l'état (allumé ou éteint) d'une LED spécifique. Les bits à 1
-    indiquent d'allumer la LED correspondante, tandis que les bits à 0 indiquent
-    de l'éteindre. La fonction utilise ce masque pour contrôler l'état de chaque
+    Cette fonction prend en entrÃ©e un masque de bits (ChoixLed) oÃ¹ chaque bit
+    correspond Ã  l'Ã©tat (allumÃ© ou Ã©teint) d'une LED spÃ©cifique. Les bits Ã  1
+    indiquent d'allumer la LED correspondante, tandis que les bits Ã  0 indiquent
+    de l'Ã©teindre. La fonction utilise ce masque pour contrÃ´ler l'Ã©tat de chaque
     LED individuellement.
 
   Remarques :
-    Les LEDs sont identifiées de BSP_LED_0 à BSP_LED_7.
-    Le masque ChoixLed permet de sélectionner quelles LEDs doivent être allumées.
-    Les LEDs non sélectionnées sont éteintes.
+    Les LEDs sont identifiÃ©es de BSP_LED_0 Ã  BSP_LED_7.
+    Le masque ChoixLed permet de sÃ©lectionner quelles LEDs doivent Ãªtre allumÃ©es.
+    Les LEDs non sÃ©lectionnÃ©es sont Ã©teintes.
 
     Exemple :
     - Pour allumer BSP_LED_0 et BSP_LED_3, ChoixLed = 0x09 (00001001 en binaire).
-    - Pour éteindre toutes les LEDs, ChoixLed = 0x00.
+    - Pour Ã©teindre toutes les LEDs, ChoixLed = 0x00.
     - Pour allumer toutes les LEDs, ChoixLed = 0xFF.
 */
 // *****************************************************************************
@@ -236,24 +236,24 @@ void APP_Initialize ( void )
 // Fonction :
 // void APP_Tasks(void)
 
-// Résumé :
-// Fonction principale de gestion des tâches de l'application.
+// RÃ©sumÃ© :
+// Fonction principale de gestion des tÃ¢ches de l'application.
 
 // Description :
-// La fonction APP_Tasks gère les différentes tâches de l'application en fonction
-// de l'état actuel. Elle comprend l'initialisation, la réception de paramètres,
-// la lecture du potentiomètre, l'affichage, l'exécution PWM, la gestion du moteur,
-// l'envoi de valeurs et la mise à jour de l'état.
+// La fonction APP_Tasks gÃ¨re les diffÃ©rentes tÃ¢ches de l'application en fonction
+// de l'Ã©tat actuel. Elle comprend l'initialisation, la rÃ©ception de paramÃ¨tres,
+// la lecture du potentiomÃ¨tre, l'affichage, l'exÃ©cution PWM, la gestion du moteur,
+// l'envoi de valeurs et la mise Ã  jour de l'Ã©tat.
 
 void APP_Tasks(void)
 {
     uint8_t CommStatus;
     static uint8_t IcycleTx;
 
-    /* Vérifier l'état actuel de l'application. */
+    /* VÃ©rifier l'Ã©tat actuel de l'application. */
     switch (appData.state)
     {
-        /* État initial de l'application. */
+        /* Ã‰tat initial de l'application. */
         case APP_STATE_INIT:
         {
             /* Initialisation de l'affichage LCD */
@@ -267,7 +267,7 @@ void APP_Tasks(void)
             printf_lcd("Damien Bignens"); 
             lcd_bl_on();
             
-            /* Initialisations des périphériques */
+            /* Initialisations des pÃ©riphÃ©riques */
             GPWM_Initialize(&PwmData);
             
             // Initialisation de l'ADC
@@ -276,13 +276,13 @@ void APP_Tasks(void)
             // Initialisation de UART (USART_ID_1)
             DRV_USART0_Initialize();
             
-            // Initialisation de la communication sérielle           
+            // Initialisation de la communication sÃ©rielle           
             InitFifoComm();
                     
-            // Mettre à jour l'état 
+            // Mettre Ã  jour l'Ã©tat 
             APP_UpdateState(APP_STATE_WAIT);
             
-            /* Toutes les LED allumées */
+            /* Toutes les LED allumÃ©es */
             EteindreLEDS();
             
             break;
@@ -290,10 +290,10 @@ void APP_Tasks(void)
 
         case APP_STATE_SERVICE_TASKS:
         {
-            // Réception des paramètres distants
+            // RÃ©ception des paramÃ¨tres distants
             CommStatus = GetMessage(&PwmData);
             
-            // Lecture de la position du potentiomètre
+            // Lecture de la position du potentiomÃ¨tre
             if (CommStatus == 0) // Local?
             {
                 GPWM_GetSettings(&PwmData); // Local
@@ -303,19 +303,19 @@ void APP_Tasks(void)
                 GPWM_GetSettings(&PwmDataToSend); // Remote
             }
             
-            // Affichage des paramètres
+            // Affichage des paramÃ¨tres
             GPWM_DispSettings(&PwmData, CommStatus);
             
-            // Exécution de la PWM et gestion du moteur
+            // ExÃ©cution de la PWM et gestion du moteur
             GPWM_ExecPWM(&PwmData);
             
-            // Effectue l'envoi après 5 cycles
+            // Effectue l'envoi aprÃ¨s 5 cycles
             if (IcycleTx >= 5)
             {
                 // Envoi des valeurs
                 if (CommStatus == 0) // Local?
                 {
-                    // SendMessage(&PwmData); // Local
+                    SendMessage(&PwmData); // Local
                 }
                 else 
                 {
@@ -323,9 +323,9 @@ void APP_Tasks(void)
                 }
             }
             
-            IcycleTx++; // Incrémentation du compteur Cycle
+            IcycleTx++; // IncrÃ©mentation du compteur Cycle
 
-            // Mettre à jour l'état 
+            // Mettre Ã  jour l'Ã©tat 
             APP_UpdateState(APP_STATE_WAIT);
             
             break;
@@ -333,11 +333,11 @@ void APP_Tasks(void)
 
         case APP_STATE_WAIT:
         {
-            // État d'attente, aucune action particulière ici
+            // Ã‰tat d'attente, aucune action particuliÃ¨re ici
             break;
         }
 
-        /* L'état par défaut ne devrait jamais être exécuté. */
+        /* L'Ã©tat par dÃ©faut ne devrait jamais Ãªtre exÃ©cutÃ©. */
         default:
         {
             break;

cc8bdde4d9a6f09cd1b329c394836b3934e94305
JoaoManuelMarquesChaves <119414658+JoaoManuelMarquesChaves@users.noreply.github.com>
2024-06-05 17:33:48 +0200

    issue gestPWM.h

--- TP2_UART/TP1_PWM 2/firmware/src/gestPWM.h
+++ TP2_UART/TP1_PWM 2/firmware/src/gestPWM.h
@@ -14,20 +14,18 @@
 /*--------------------------------------------------------*/
 
 #include <stdint.h>
-#include "app.h"
 #include<math.h>
-#include "peripheral/oc/plib_oc.h"
 
 /*--------------------------------------------------------*/
-// Définition des fonctions prototypes
+// DÃ©finition des fonctions prototypes
 /*--------------------------------------------------------*/
 #define TAILLE_MOYENNE_ADC 10  
 
 typedef struct {
-    uint8_t absSpeed;    // vitesse 0 à 99
-    uint8_t absAngle;    // Angle  0 à 180
-    int8_t SpeedSetting; // consigne vitesse -99 à +99
-    int8_t AngleSetting; // consigne angle  -90 à +90
+    uint8_t absSpeed;    // vitesse 0 Ã  99
+    uint8_t absAngle;    // Angle  0 Ã  180
+    int8_t SpeedSetting; // consigne vitesse -99 Ã  +99
+    int8_t AngleSetting; // consigne angle  -90 Ã  +90
 } S_pwmSettings;
 
 extern S_pwmSettings PwmData;
@@ -35,7 +33,7 @@ extern S_pwmSettings PwmDataToSend;
 
 void GPWM_Initialize(S_pwmSettings *pData);
 
-// Ces 3 fonctions ont pour paramètre un pointeur sur la structure S_pwmSettings.
+// Ces 3 fonctions ont pour paramÃ¨tre un pointeur sur la structure S_pwmSettings.
 void GPWM_GetSettings(S_pwmSettings *pData);	// Obtention vitesse et angle
 void GPWM_DispSettings(S_pwmSettings *pData, int Remote);	// Affichage
 void GPWM_ExecPWM(S_pwmSettings *pData);		// Execution PWM et gestion moteur.
